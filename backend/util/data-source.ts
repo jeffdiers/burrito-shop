a417d5929941047e9a96fb29a07553f9c8997652
Jeff Diers <jeff.diers@gmail.com>
2023-10-16 10:13:40 -0400

    create docker-compose to build frontend and backend seperate

--- backend/util/data-source.ts
+++ backend/util/data-source.ts
@@ -13,8 +13,8 @@ const DATABASE =
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: DATABASE,
